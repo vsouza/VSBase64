356f8304ca2e282178afb6343778286ced202338
Vinicius Souza <vinicius@codemakers.com.br>
2017-02-01 15:55:30 -0200

    fix tests

--- Tests/VSBase64Tests/VSBase64Test.swift
+++ Tests/VSBase64Tests/VSBase64Test.swift
@@ -3,6 +3,14 @@ import XCTest
 
 class TestVSBase64: XCTestCase {
 
+
+    static var allTests: [(String, (TestVSBase64) -> () throws -> Void)] {
+        return [
+            ("testEncode", testEncode),
+            ("testDecode", testDecode),
+        ]
+    }
+
     func testEncode(){
         let encode = VSBase64.encode(str: "foo")
         XCTAssertEqual(encode, "Zm9v")
